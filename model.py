0811e4ae43db768a48deff3fbd1096905e34be11
Aniket1998 <aniketd@iitk.ac.in>
2018-12-03 19:56:26 +0530

    Change factorised inference model to match the way its defined in the paper

--- model.py
+++ model.py
@@ -5,9 +5,10 @@ import torch.nn.functional as F
 __all__ = ['DisentangledVAE']
 
 
+# Differences from original paper : Step up the number of filters in multiples of 64
 class DisentangledVAE(nn.Module):
-    def __init__(self, f_dim=64, z_dim=32, conv_dim=1024, hidden_dim=512,
-                 frames=8, filters=256, p_drop=0.5, nonlinearity=None, factorised=False):
+    def __init__(self, f_dim=64, z_dim=32, conv_dim=1024, step=64, in_size=64, hidden_dim=512,
+                 frames=8, nonlinearity=None, factorised=False):
         super(DisentangledVAE, self).__init__()
         self.f_dim = f_dim
         self.z_dim = z_dim
@@ -15,81 +16,68 @@ class DisentangledVAE(nn.Module):
         self.conv_dim = conv_dim
         self.hidden_dim = hidden_dim
         self.factorised = factorised
-        self.filters=filters
+        self.step = step
+        self.in_size = in_size
         nl = nn.LeakyReLU(0.2) if nonlinearity is None else nonlinearity
 
         self.f_lstm = nn.LSTM(self.conv_dim, self.hidden_dim, 1,
                               bidirectional=True, batch_first=True)
-        self.f_mean = nn.Sequential(
-                nn.Dropout(p_drop),
-                nn.Linear(self.hidden_dim * 2, self.f_dim))
-        self.f_logvar = nn.Sequential(
-                nn.Dropout(p_drop),
-                nn.Linear(self.hidden_dim * 2, self.f_dim)
-                )
+        self.f_mean = nn.Sequential(nn.Linear(self.hidden_dim * 2, self.f_dim), nl)
+        self.f_logvar = nn.Sequential(nn.Linear(self.hidden_dim * 2, self.f_dim), nl)
 
         if self.factorised is True:
-            self.z_lstm = nn.LSTM(self.conv_dim, self.hidden_dim, 1,
-                                  bidirectional=True, batch_first=True)
+            self.z_mean = nn.Sequential(nn.Linear(self.conv_dim, self.z_dim), nl)
+            self.z_logvar = nn.Sequential(nn.Linear(self.conv_dim, self.z_dim), nl)
         else:
-            self.z_lstm = nn.LSTM(self.conv_dim+self.f_dim, self.hidden_dim, 1,
-                                  bidirectional=True, batch_first=True)
+            self.z_lstm = nn.LSTM(self.conv_dim + self.f_dim, self.hidden_dim, 1, bidirectional=True, batch_first=True)
+            self.z_rnn = nn.RNN(self.hidden_dim * 2, self.hidden_dim, batch_first=True)
+            self.z_mean = nn.Sequential(nn.Linear(self.hidden_dim, self.z_dim), nl)
+            self.z_logvar = nn.Sequential(nn.Linear(self.hidden_dim, self.z_dim), nl)
 
-        self.z_rnn = nn.RNN(self.hidden_dim*2, self.hidden_dim, batch_first=True)
+        encoding_conv = []
+        channels = step
+        encoding_conv.append(nn.Sequential(nn.Conv2d(3, channels, 5, 4, 1, bias=False), nl))
+        size = in_size // 4
+        while size > 4:
+            encoding_conv.append(nn.Sequential(
+                nn.Conv2d(channels, channels * 2, 5, 4, 1, bias=False),
+                nn.BatchNorm2d(channels * 2), nl))
+            size = size // 4
+            channels *= 2
 
-        self.z_mean = nn.Sequential(
-                nn.Dropout(p_drop),
-                nn.Linear(self.hidden_dim, self.z_dim)
-                )
-        self.z_logvar = nn.Sequential(
-                nn.Dropout(p_drop),
-                nn.Linear(self.hidden_dim, self.z_dim)
-                )
+        self.encode_final_size = size
+        self.encode_final_channels = channels
 
-        self.conv = nn.Sequential(
-                nn.Conv2d(3, filters, 4, 2, 1, bias=True), nl,
-                nn.Conv2d(filters, filters, 4, 2, 1, bias=False),
-                nn.BatchNorm2d(filters), nl,
-                nn.Dropout2d(p_drop),
-                nn.Conv2d(filters, filters, 4, 2, 1, bias=False),
-                nn.BatchNorm2d(filters), nl,
-                nn.Dropout2d(p_drop),
-                nn.Conv2d(filters, filters, 4, 2, 1, bias=False),
-                nn.BatchNorm2d(filters), nl,
-                nn.Dropout2d(p_drop))
-
+        self.conv = nn.Sequential(*encoding_conv)  # Common to q(f|x1:T) and q(z1:T)
         self.conv_fc = nn.Sequential(
-                nn.Linear(4*4*filters, self.conv_dim),
-                nn.BatchNorm1d(self.conv_dim), nl)
+                nn.Linear(size * size * channels, conv_dim),
+                nn.BatchNorm1d(conv_dim), nl)
 
-        self.deconv_fc = nn.Sequential(
-                nn.Linear(self.f_dim+self.z_dim, 4*4*filters),
-                nn.BatchNorm1d(4*4*filters), nl)
-
-        self.deconv = nn.Sequential(
-                nn.ConvTranspose2d(filters, filters, 4, 2, 1, bias=False),
-                nn.BatchNorm2d(filters), nl,
-                nn.Dropout2d(p_drop),
-                nn.ConvTranspose2d(filters, filters, 4, 2, 1, bias=False),
-                nn.BatchNorm2d(filters), nl,
-                nn.Dropout2d(p_drop),
-                nn.ConvTranspose2d(filters, filters, 4, 2, 1, bias=False),
-                nn.BatchNorm2d(filters), nl,
-                nn.Dropout2d(p_drop),
-                nn.ConvTranspose2d(filters, 3, 4, 2, 1, bias=True), nn.Tanh())
-
+        self.deconv_fc = nn.Sequential(nn.Linear(self.f_dim + self.z_dim, 4 * 4 * step), nl)
+        decoding_conv = []
+        channels = step
+        size = 4
+        while size < in_size // 4:
+            decoding_conv.append(nn.Sequential(
+                nn.ConvTranspose2d(channels, channels * 2, 5, 4, 1, 1),
+                nn.BatchNorm2d(channels * 2), nl))
+            channels *= 2
+            size *= 4
+        decoding_conv.append(nn.Sequential(
+            nn.ConvTranspose2d(channels, 3, 5, 4, 1, 1), nn.Tanh()))
+        self.deconv = nn.Sequential(*decoding_conv)
         for m in self.modules():
             if isinstance(m, nn.BatchNorm2d) or isinstance(m, nn.BatchNorm1d):
                 nn.init.constant_(m.weight, 1)
                 nn.init.constant_(m.bias, 1)
             elif isinstance(m, nn.Conv2d) or isinstance(m, nn.ConvTranspose2d) or isinstance(m, nn.Linear):
                 nn.init.kaiming_normal_(m.weight)
-        nn.init.xavier_normal_(self.deconv[len(self.deconv) - 2].weight, nn.init.calculate_gain('tanh'))
+#        nn.init.xavier_normal_(self.deconv[len(self.deconv) - 2].weight, nn.init.calculate_gain('tanh'))
 
     def encode_frames(self, x):
-        x = x.view(-1, 3, 64, 64)
+        x = x.view(-1, 3, self.in_size, self.in_size)
         x = self.conv(x)
-        x = x.view(-1, 4*4*self.filters)
+        x = x.view(-1, self.encode_final_channels * (self.encode_final_size ** 2))
         x = self.conv_fc(x)
         x = x.view(-1, self.frames, self.conv_dim)
         return x
@@ -97,10 +85,9 @@ class DisentangledVAE(nn.Module):
     def decode_frames(self, zf):
         x = zf.view(-1, self.f_dim+self.z_dim)
         x = self.deconv_fc(x)
-        x = x.view(-1, self.filters, 4, 4)
+        x = x.view(-1, self.step, 4, 4)
         x = self.deconv(x)
-        print(x.shape)
-        return x.view(-1, self.frames, 3, 64, 64)
+        return x.view(-1, self.frames, 3, self.in_size, self.in_size)
 
     def reparameterize(self, mean, logvar):
         if self.training:
@@ -119,13 +106,13 @@ class DisentangledVAE(nn.Module):
 
     def encode_z(self, x, f):
         if self.factorised is True:
-            lstm_out, _ = self.z_lstm(x)
+            features = x
         else:
             f_expand = f.unsqueeze(1).expand(-1, self.frames, self.f_dim)
             lstm_out, _ = self.z_lstm(torch.cat((x, f_expand), dim=2))
-        rnn_out, _ = self.z_rnn(lstm_out)
-        mean = self.z_mean(rnn_out)
-        logvar = self.z_logvar(rnn_out)
+            features, _ = self.z_rnn(lstm_out)
+        mean = self.z_mean(features)
+        logvar = self.z_logvar(features)
         return mean, logvar, self.reparameterize(mean, logvar)
 
     def forward(self, x):
